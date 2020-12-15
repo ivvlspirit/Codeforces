6a6b187386a1bb10958cee184d5a78afac03e5be
Ivan Vlahov <ivan.vlahov@gmail.com>
2020-12-15 23:46:05 +0100

    Added my CF template

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+const int MOD = 1e9+7;
+
+int main(){
+
+    #ifdef DEBUG
+        freopen("..\\input.txt", "r", stdin);
+        clock_t tStart = clock();
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    for(; t; t--){
+
+
+
+        cout << "\n";
+    }
+
+    #ifdef DEBUG
+        printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    #endif
+
+    return 0;
+}
